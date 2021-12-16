4ea8358f181cbe6e53889bbe585f7b8c6cf90e28
bsarasuspe <83415989+bsarasuspe@users.noreply.github.com>
2021-12-16 11:51:04 +0100

    Login/Register hasi

--- src/bean/eredua/FacadeBean.java
+++ src/bean/eredua/FacadeBean.java
@@ -21,7 +21,7 @@ public class FacadeBean {
 	private FacadeBean() {
 		try {
 			facadeInterface = new BLFacadeImplementation(new HibernateDataAccess());
-			facadeInterface.initializeBD();
+			//facadeInterface.initializeBD();
 		} catch (Exception e) {
 			System.out.println("FacadeBean: negozioaren logika sortzean errorea: " + e.getMessage());
 		}
---
+++ src/domain/User.java
@@ -0,0 +1,42 @@
+package domain;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class User {
+	@Id
+	private String izena;
+	private String pasahitza;
+
+	public User() {
+	}
+
+	public String getIzena() {
+		return izena;
+	}
+
+	public void setIzena(String izena) {
+		this.izena = izena;
+	}
+
+	public String getPasahitza() {
+		return pasahitza;
+	}
+
+	public void setPasahitza(String pasahitza) {
+		this.pasahitza = pasahitza;
+	}
+
+	public String toString() { // Erabiltzailea
+		return izena + "/" + pasahitza;
+	}
+
+}
